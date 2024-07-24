02c24f4e464b936f548a213c18d4c2a402710d38
Tiago Martins <tiagomartinscc@hotmail.com>
2024-07-24 07:05:16 -0300

    fix path db name

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,11 @@
+import path from 'node:path'
+
 import { knex as setupKnex, Knex } from 'knex'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db',
+    filename: path.resolve('./db/app.db'),
   },
   useNullAsDefault: true,
   migrations: {
