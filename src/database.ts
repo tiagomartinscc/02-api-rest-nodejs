421ac3f26eea5b34c6f70e340d9a9763e520d091
Tiago Martins <tiagomartinscc@hotmail.com>
2024-07-26 21:18:47 -0300

    create a transaction

--- src/database.ts
+++ src/database.ts
@@ -1,18 +1,16 @@
-import path from 'node:path'
-
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: path.resolve(env.DATABASE_URL),
+    filename: env.DATABASE_URL,
   },
-  useNullAsDefault: true,
   migrations: {
     extension: 'ts',
     directory: './db/migrations',
   },
+  useNullAsDefault: true,
 }
 
 export const knex = setupKnex(config)
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
-
-app.get('/hello', async () => {
-  const transactions = await knex('transactions').select('*')
-
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
