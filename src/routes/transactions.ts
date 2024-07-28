07fd46e313ef5d1ceff01c9dda3c3969139a95fe
Tiago Martins <tiagomartinscc@hotmail.com>
2024-07-28 11:41:23 -0300

    create and save sessionId into transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -17,10 +17,10 @@ export async function transactionsRoutes(app: FastifyInstance) {
   })
 
   app.get('/:id', async (request) => {
-    const getTransactionZeodSchema = z.object({
+    const getTransactionZodSchema = z.object({
       id: string().uuid(),
     })
-    const { id } = getTransactionZeodSchema.parse(request.params)
+    const { id } = getTransactionZodSchema.parse(request.params)
 
     const transaction = await knex('transactions')
       .where({
@@ -39,10 +39,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body,
     )
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
